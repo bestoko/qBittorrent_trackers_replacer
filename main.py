16086cb626d3f8b096cfcbd7f1a3f9041db6e5fc
bestoko <bestokocc@gmail.com>
2024-10-28 22:06:02 +0800

    init

---
+++ main.py
@@ -0,0 +1,8 @@
+import sys
+from PyQt5.QtWidgets import *
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('PyCharm')
