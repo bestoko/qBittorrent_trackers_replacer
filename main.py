88498d480bc8f6ec5b9735bc01d6fb564c8db4b7
bestoko <bestokocc@gmail.com>
2024-10-28 22:13:42 +0800

    init ui

--- main.py
+++ main.py
@@ -3,6 +3,32 @@ from PyQt5.QtWidgets import *
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-# Press the green button in the gutter to run the script.
+class Window(QWidget):
+    def __init__(self):
+        super(Window, self).__init__()
+        self.setWindowTitle('qBittorrent trackers replacer')
+
+        bt_backup_path_label = QLabel('BT Backup path:')
+        old_tracker_label = QLabel('old tracker:')
+        new_tracker_label = QLabel('new tracker:')
+        confirm_btn = QPushButton('Confirm')
+        bt_backup_path_line = QLineEdit()
+        old_tracker_line = QLineEdit()
+        new_tracker_line = QLineEdit()
+
+
+        f_layout = QFormLayout()
+        f_layout.addRow(bt_backup_path_label, bt_backup_path_line)
+        f_layout.addRow(old_tracker_label, old_tracker_line)
+        f_layout.addRow(new_tracker_label, new_tracker_line)
+        f_layout.addRow(confirm_btn)
+        self.setLayout(f_layout)
+
+        confirm_btn.clicked.connect(lambda: confirm_btn.setText('Confirmed'))
+
+
 if __name__ == '__main__':
-    print('PyCharm')
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec())
